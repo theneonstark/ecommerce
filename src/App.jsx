f896b3e493ba342c505e85f15c301acb9f421e5a
Shahid <shahid@artheducation.com>
2024-04-25 17:34:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className='flex'>
+        <h1>Hello Store</h1>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Home</a>
+          <a href="#">Home</a>
+          <a href="#">Home</a>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default App
